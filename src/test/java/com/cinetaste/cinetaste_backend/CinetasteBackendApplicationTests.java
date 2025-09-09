6654b9dee2c5d320efffdd49b6c04d69ae38e919
huy1113 <huyndbh00930@fpt.edu.vn>
2025-09-09 09:11:18 +0700

    First commit - CineTaste project

---
+++ src/test/java/com/cinetaste/cinetaste_backend/CinetasteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinetaste.cinetaste_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinetasteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
